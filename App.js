8625820b881e784c49b5b0ba7bd6d03a8828db0a
thao <leanhthao01@gmail.com>
2019-12-18 00:58:49 +0700

    added pages

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import AccountScreen from './src/screens/AccountScreen';
+import SigninScreen from './src/screens/SigninScreen';
+import SignupScreen from './src/screens/SignupScreen';
+import TrackCreateScreen from './src/screens/TrackCreateScreen';
+import TrackDetailScreen from './src/screens/TrackDetailScreen';
+import TrackListScreen from './src/screens/TrackListScreen';
+
 export default function App() {
   return (
     <View style={styles.container}>
---
+++ src/screens/TrackCreateScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const TrackCreateScreen = () => {
+    return(
+        <View>
+            <Text style={{fontSize: 48}}>Track Create Screen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default TrackCreateScreen;
